a4ffe4e1ccb10a6ede5ee9382bf94840c34b56f4
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-23 09:21:15 +0300

    feat: create components

---
+++ src/data/sociallinks.jsx
@@ -0,0 +1,26 @@
+export const sociallinks = [
+    {
+        platform: 'Twitter Link',
+        url: 'https://twitter.com/mbiebuz'
+    },
+    {
+        platform: 'Zuri Team',
+        url: 'https://training.zuri.team/'
+    },
+    {
+        platform: 'Zuri Books',
+        url: 'https://books.zuri.team/'
+    },
+    {
+        platform: 'Python Books',
+        url: 'https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E'
+    },
+    {
+        platform: 'Background Check for Coders',
+        url: 'https://background.zuri.team/'
+    },
+    {
+        platform: 'Design Books',
+        url: 'https://books.zuri.team/design-rules'
+    },
+]
