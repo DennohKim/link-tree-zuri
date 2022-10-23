b91ecf71ba3b9673327fbde63e0b0cd87941e999
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-23 11:06:17 +0300

    feat: add id to btn link

--- src/data/sociallinks.jsx
+++ src/data/sociallinks.jsx
@@ -1,25 +1,31 @@
 export const sociallinks = [
     {
+        id: 'btn-twitter',
         platform: 'Twitter Link',
         url: 'https://twitter.com/mbiebuz'
     },
     {
+        id: 'btn__zuri',
         platform: 'Zuri Team',
         url: 'https://training.zuri.team/'
     },
     {
+        id: 'books',
         platform: 'Zuri Books',
         url: 'https://books.zuri.team/'
     },
     {
+        id: 'book__python',
         platform: 'Python Books',
         url: 'https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E'
     },
     {
+        id: 'pitch',
         platform: 'Background Check for Coders',
         url: 'https://background.zuri.team/'
     },
     {
+        id: 'book__design',
         platform: 'Design Books',
         url: 'https://books.zuri.team/design-rules'
     },
